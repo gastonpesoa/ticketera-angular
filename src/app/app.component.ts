c0717e48bc5ff49e182c8eff00a542a69bd56f64
PesoaGaston <pesoagaston@MacBook-Pro-de-PesoaGaston.local>
2021-06-16 23:37:25 -0300

    clears

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,94 +9,133 @@ import { catchError, retry } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private http: HttpClient) { 
-    //this.usedUrl=this.urlLocal;
-    this.usedUrl=this.urlServer;
+  constructor(private http: HttpClient) {
+    this.usedUrl = this.urlLocal;
+    //this.usedUrl=this.urlServer;
   }
   title = 'ticketera';
-  urlLocal="http://localhost:8080/";
-  urlServer="https://nodejs-mongodb-ticketera.herokuapp.com/";
-  usedUrl="";
-  responseDesperfectos:any;
-  responseAtenciones:any;
-  responseZonales:any;
-  responseClientes:any;
-  get(){
+  urlLocal = "http://localhost:8080/";
+  urlServer = "https://nodejs-mongodb-ticketera.herokuapp.com/";
+  usedUrl = "";
+  responseDesperfectos: any;
+  responseAtenciones: any;
+  responseZonales: any;
+  responseClientes: any;
+  get() {
     return this.http.get(this.usedUrl)
-      .subscribe((data:any) => {
-        this.responseDesperfectos = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseDesperfectos = JSON.stringify(data)
+        this.responseAtenciones = ""
+        this.responseZonales = ""
+        this.responseClientes = ""
       })
   }
-  getDesperfectos(){
+  getDesperfectos() {
     return this.http.get(this.usedUrl.concat("desperfectos"))
-      .subscribe((data:any) => {
-        this.responseDesperfectos = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseDesperfectos = JSON.stringify(data)
+        this.responseAtenciones = ""
+        this.responseZonales = ""
+        this.responseClientes = ""
       })
   }
-  getDesperfectosDondeOcurren(){
+  getDesperfectosDondeOcurren() {
     return this.http.get(this.usedUrl.concat("desperfectos-donde"))
-      .subscribe((data:any) => {
-        this.responseDesperfectos = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseDesperfectos = JSON.stringify(data)
+        this.responseAtenciones = ""
+        this.responseZonales = ""
+        this.responseClientes = ""
       })
   }
-  getDesperfectosCadaCuanto(){
+  getDesperfectosCadaCuanto() {
     return this.http.get(this.usedUrl.concat("desperfectos-cada-cuanto"))
-      .subscribe((data:any) => {
-        this.responseDesperfectos = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseDesperfectos = JSON.stringify(data)
+        this.responseAtenciones = ""
+        this.responseZonales = ""
+        this.responseClientes = ""
       })
   }
-  getAtencionEmpleado(){
+  getAtencionEmpleado() {
     return this.http.get(this.usedUrl.concat("atencion-empleado"))
-      .subscribe((data:any) => {
-        this.responseAtenciones = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseAtenciones = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseZonales = ""
+        this.responseClientes = ""
       })
   }
-  getAtencionHorario(){
+  getAtencionHorario() {
     return this.http.get(this.usedUrl.concat("atencion-horario"))
-      .subscribe((data:any) => {
-        this.responseAtenciones = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseAtenciones = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseZonales = ""
+        this.responseClientes = ""
       })
   }
-  getAtencionTrabajos(){
+  getAtencionTrabajos() {
     return this.http.get(this.usedUrl.concat("atencion-trabajos"))
-      .subscribe((data:any) => {
-        this.responseAtenciones = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseAtenciones = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseZonales = ""
+        this.responseClientes = ""
       })
   }
-  getZonalesDesperfectos(){
+  getZonalesDesperfectos() {
     return this.http.get(this.usedUrl.concat("zonales-desperfectos"))
-      .subscribe((data:any) => {
-        this.responseZonales = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseZonales = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseAtenciones = ""
+        this.responseClientes = ""
       })
   }
-  getZonalesAtenciones(){
+  getZonalesAtenciones() {
     return this.http.get(this.usedUrl.concat("zonales-atenciones"))
-      .subscribe((data:any) => {
-        this.responseZonales = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseZonales = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseAtenciones = ""
+        this.responseClientes = ""
       })
   }
-  getClientesConsultas(){
+  getClientesConsultas() {
     return this.http.get(this.usedUrl.concat("clientes-consultas"))
-      .subscribe((data:any) => {
-        this.responseClientes = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseClientes = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseAtenciones = ""
+        this.responseZonales = ""
       })
   }
-  getClientesTickets(){
+  getClientesTickets() {
     return this.http.get(this.usedUrl.concat("clientes-tickets"))
-      .subscribe((data:any) => {
-        this.responseClientes = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseClientes = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseAtenciones = ""
+        this.responseZonales = ""
       })
   }
-  getClientesZona(){
+  getClientesZona() {
     return this.http.get(this.usedUrl.concat("clientes-zona"))
-      .subscribe((data:any) => {
-        this.responseClientes = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseClientes = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseAtenciones = ""
+        this.responseZonales = ""
       })
   }
-  getClientesEmpleados(){
+  getClientesEmpleados() {
     return this.http.get(this.usedUrl.concat("clientes-empleados"))
-      .subscribe((data:any) => {
-        this.responseClientes = JSON.stringify(data)        
+      .subscribe((data: any) => {
+        this.responseClientes = JSON.stringify(data)
+        this.responseDesperfectos = ""
+        this.responseAtenciones = ""
+        this.responseZonales = ""
       })
   }
 }
